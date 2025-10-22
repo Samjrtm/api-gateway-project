45227fc742a40645cee1699c393a1fa603052bf7
Samjrtm <samuelwengtimothy@gmail.com>
2025-10-22 11:16:15 +0400

    Port Update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 8000,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
